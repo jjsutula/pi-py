5c36223b000863421906fdda53d0d82c44dbc8ea
jjsutula <jsutula@workpoint.com>
2019-06-30 16:52:39 -0500

    Use gpio library recommended on the raspberry pi site: https://gpiozero.readthedocs.io/en/stable/

--- blink/blink.py
+++ blink/blink.py
@@ -1,15 +1,26 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED
 from time import sleep
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.out)
-print("Python: Time to blink.")
-for i in range(1, 10):
-    GPIO.output(17, GPIO.HIGH)
+led = LED(17)
+
+while True:
+    led.on()
     sleep(1)
-    GPIO.output(17, GPIO.LOW)
-    sleep(0.5)
-GPIO.cleanup()
+    led.off()
+    sleep(1)
+
+# import RPi.GPIO as GPIO
+# from time import sleep
+#
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(17, GPIO.out)
+# print("Python: Time to blink.")
+# for i in range(1, 10):
+#     GPIO.output(17, GPIO.HIGH)
+#     sleep(1)
+#     GPIO.output(17, GPIO.LOW)
+#     sleep(0.5)
+# GPIO.cleanup()
 
 # Do this so that main() only gets called if invoked via command line, but not if invoked programmatically
 if __name__ == "__main__":
